c786f378459698cf522d2b423e75c85283942b29
PedrouColares <pedro.colaressrubens15789@gmail.com>
2025-09-18 17:38:16 -0300

    added cv

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -17,7 +17,7 @@ export default function Profile() {
                                 <div className="inline-flex flex-row items-center w-max xl:gap-2 gap-1 text-gray-600 hover:text-black mb-2 text-left whitespace-nowrap cursor-pointer">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-newspaper-icon lucide-newspaper"><path d="M15 18h-5"/><path d="M18 14h-8"/><path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-4 0v-9a2 2 0 0 1 2-2h2"/><rect width="8" height="4" x="10" y="6" rx="1"/></svg>
                                     <p className="text-gray-500 mx-0">&middot;</p>
-                                    <span><a target="" href="">Currículo</a></span>
+                                    <span><a target="" href="https://drive.google.com/file/d/13Ba3GrTBlexk7DJb1CX90kRI3YOHCn58/view?usp=sharing">Currículo</a></span>
                                 </div>
                             </TooltipTrigger>
                         </Tooltip>
